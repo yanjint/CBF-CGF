6fa2afa41794ddc13a33bb1ef0da7e23ca03ef9f
yanjint <yanjintan7879@gmail.com>
2025-05-22 19:16:36 +0800

    update files

--- Funs/main_max.m
+++ Funs/main_max.m
@@ -1,33 +1,29 @@
-%% 灏嗗嵎绉牳涓庨噸鏋勫浘鍒嗗紑瀛︿範
 function [Y_graph, y0, it, obj] = main_max(KF, Graph,c, dim_W, niter, evs,lambda)
-num = size(KF{1}, 1);   % 210
-m = size(KF, 1);     %5      
-n = length(Graph);      %number of graphs
+num = size(KF{1}, 1);   
+m = size(KF, 1);     
+n = length(Graph);     
 dim_F = size(KF{1}, 2);
 
-% 文件写入
-% fid=fopen('./record.txt','w+');
-% fprintf(fid,'%s\n',string(5));
-% fclose(fid);
+
 
 %% init
-Y = full(Init_Y(Graph, c)); %得到指示矩阵
-[~, y0] = max(Y, [], 2);  %y0为簇号
-%W的初始化
+Y = full(Init_Y(Graph, c)); %Indicator matrices
+[~, y0] = max(Y, [], 2);  %
+%
 W = cell(m, m);
 for i = 1 : m
     for j = 1 : m
-        W{i,j} = eye(dim_F, dim_W) / m; %在分块矩阵中，m^2*c的情况下，单位正交 
+        W{i,j} = eye(dim_F, dim_W) / m; 
     end 
 end
 
-a = ones(n + 1) / sqrt(n+1);   %论文中的q
+a = ones(n + 1) / sqrt(n+1);   
 a(end)=lambda;
 
-% 初始化h
+%h
 h = ones(n) / sqrt(n);
 
-%初始化θ
+%θ
 T = ones(1,2*c);
 T_norm = T ./ sqrt(2*c);
 M = T_norm(1:c);
@@ -38,10 +34,10 @@ sita = cell(1,2);
 sita{1} = M;
 sita{2} = N;
 
-% 初始化p
+% p
 p = ones(m, m) ./ sqrt(m * m);
 
-%初始化Y
+%Y
 YYYY = (Y * (Y' * Y*sita{1})^(-1)) * Y';
 p_flatten = reshape(p, [1, m * m]); % 直接初始化的Vec(p)
 Q = zeros(num);  % \hat(A)
@@ -62,7 +58,7 @@ for i = i :n
     S2 = S2 + Graph{i}*h(i);
 end
 
-% fid=fopen('./ORL.txt','w+');
+
 for it=1:niter    % 40  
     %% update W
     % KF是列标准化的U
@@ -70,7 +66,7 @@ for it=1:niter    % 40
     XX = reshape(KF, [1, m * m]);
     K = [];
     for i = 1 : m * m
-        K = [K, XX{i}]; %没有p的U
+        K = [K, XX{i}]; %
     end
     H = [];
     for i = 1 : m * m
@@ -121,7 +117,7 @@ for it=1:niter    % 40
     
   
     %% update p
-    Q = zeros(num); %hat_A
+    Q = zeros(num); 
     for i = 1 : n  
         Q = Q + Graph{i} * a(i);
     end
@@ -130,14 +126,11 @@ for it=1:niter    % 40
     D = cell(m, m);
     for i = 1 : m
         for j = 1 : m
-            D{i,j} = KF{i, j} * W{i, j}; %UW
+            D{i,j} = KF{i, j} * W{i, j}; %
         end
     end
     D_flatten = reshape(D, [1, m * m]);
-%     U = cell(1, m * m);
-%     for i = 1 : m * m
-%         U{i} = Q * D_flatten{i};
-%     end
+
     U = cellfun(@(x) Q * x, D_flatten, 'UniformOutput', 0);
     A = Vec(D_flatten);
     B = Vec(U);
@@ -150,12 +143,12 @@ for it=1:niter    % 40
     G = zeros(num, dim_W);
     for i = 1 : m
         for j = 1 : n
-            G = G + p(i,j) * KF{i,j} * W{i,j}; %U_p*W，谱嵌入层面
+            G = G + p(i,j) * KF{i,j} * W{i,j}; 
         end
     end
     
-    %得到原图层面的嵌入
-    S2 = zeros(num); %hat_A
+
+    S2 = zeros(num); 
     for i = 1 : n  
         S2 = S2 + Graph{i} * h(i);
     end
@@ -166,21 +159,19 @@ for it=1:niter    % 40
     end
     KK = (KK+KK')/2;
 
-    %Y = CDKM(G*G', Y,S2,sita);
+
     Y = solve_F(KK, Y);
-    %Y = full(ind2vec(y'))';
+
     [~, Y_1] = max(Y, [], 2);
-%     Y = coordinate_descend(G * G', Y);
+
     YYYY = (Y * (Y' * Y*sita{1})^(-1)) * Y'+(Y * (Y' * Y*sita{2})^(-1)) * Y' ;
      obj(it) = get_obj(Y, a, p, W, KF, Graph,h,sita,lambda);
-     %fprintf(fid,'%s\n',string(obj(it)));
    if it>2 && (obj(it) -obj(it-1)) / obj(it-1) < 1e-4
        break
    end
 end
 
 [~, Y_graph] = max(Y, [], 2);
-% fclose(fid);
 end
 
 
@@ -195,15 +186,15 @@ end
 
 
 function obj = get_obj(Y, a, p, W, KF, Graph,h,sita,lambda)
-m = size(KF, 1); %5
-num = size(KF{1}, 1); %210
+m = size(KF, 1); 
+num = size(KF{1}, 1); 
 G = zeros(num, size(W{1}, 2));
 for i = 1 : m
     for j = 1 : m
         G = G + p(i,j) * KF{i,j} * W{i,j};
     end
 end
-%原图层面
+
 Q = zeros(num);
 for i = 1 : length(Graph)
     Q = Q + Graph{i} * a(i);
