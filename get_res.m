ccad436cbd8b7070a6f9a81b4410baf8b4677b32
yanjint <yanjintan7879@gmail.com>
2025-05-22 19:13:02 +0800

    Update get_res.m

--- get_res.m
+++ get_res.m
@@ -6,33 +6,22 @@ warning off
 addpath(genpath('./datasets'))
 addpath('./Funs');
 addpath('./finchpp')
-data = {'MSRC-v6'};  % MSRC  ORL  WikipediaArticles_total
-orders = [1];    % 图滤波器
-k = 5;           % 5阶稀疏构图 
+data = {'ORL'};  
+orders = [5];    % filter
+k = 5;           % o
 lambda = [1]
 for i= 1:length(lambda)
     for idx = 1: length(data) 
         load(data{idx})
-    %%  运行数据据Yale_32x32，COIL20添加 
-%             X = cell(1,2);
-%             X{1} = feature;
-%             X{1} = feature;
-%             Y = label';
-    %
-    %针对Mfeat
-%         X = data;
-%         Y = truelabel{1};
-%         for t=1:length(X)
-%             X{t}=data{t}';
-%         end
+
         for j=1:length(X)
-            colnum=size(X{j},2);           % colnum = 24
+            colnum=size(X{j},2);          
             mole = repmat(std(X{j},0,2),1,colnum);
             mole(mole==0) = 1; 
             X{j}=(X{j}-repmat(mean(X{j},2),1,colnum))./mole;
         end
         
-        c = length(unique(Y));   % 类别 classes = 7
+        c = length(unique(Y));   
         [G, K, F, evs]=prepare(X, k, c);
         KF = cross_con(K, F, 1);
         tic;
